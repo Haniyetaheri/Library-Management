195f6cd4aeca2c0c2756dce65e2466509abca722
Haniyetaheri <65609519+Haniyetaheri@users.noreply.github.com>
2022-06-21 12:33:37 +0430

    Create Erore.h

---
+++ Erore.h
@@ -0,0 +1,13 @@
+#ifndef ERORE_H
+#define ERORE_H
+#include<string>
+class Erore//the class for erore of proje
+{
+public:
+	Erore(std::string mas = "");
+	void get_massae();
+
+private:
+	std::string massae;
+};
+#endif // !ERORE
